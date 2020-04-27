3cb03fafec70318a33fb46081c65c0a34357db3e
jmcab95 <josemicabanillasmansilla@gmail.com>
2020-04-27 19:21:04 +0200

    Update Project with mbed_app.json

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "mbed.h"
+#include "USBSerial.h"
+
+
+Serial pcDebug(USBTX,USBRX,115200);
+USBSerial usbSerial;
+
 
 // main() runs in its own thread in the OS
 int main()
 {
     while (true) {
-
+     pcDebug.printf("Im SPLink Serial Console \r\n");
+     usbSerial.printf("Im virtual usb Serial port \r\n");
+    
     }
 }
+
 
