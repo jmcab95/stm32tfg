0746f327c17df26c6e6c5159ab0c3ac08421277f
jmcab95 <josemicabanillasmansilla@gmail.com>
2020-06-17 15:08:42 +0200

    UPdate

--- main.cpp
+++ main.cpp
@@ -4,23 +4,12 @@
 
 
 //Declare Serial Uart
+
 Serial pcDebug(USBTX,USBRX);
 //Declare USBSerial Device
-//USBSerial usbSerial;
-//Declare USBHID device con el tamaño de input y output 8 bytes 
+USBSerial usbSerial;
 
-USBHID hid(8, 8);
-
-//Declaro estructuras de entrada y salida
-HID_REPORT output_report ={
-    .length = 8,
-    .data={0}
-};
-HID_REPORT input_report ={
-    .length = 0,
-    .data = {0}
-};
-
+char name = 'H';
 
 
 
@@ -28,30 +17,11 @@ HID_REPORT input_report ={
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
-     //pcDebug.printf("Im SPLink Serial Console \r\n");
-     //usbSerial.printf("Im virtual usb Serial port \r\n");
-    //Filling report 
-
-    for(int i = 0; i<output_report.length;i++){
-        output_report.data[i] = rand();
-    }
-
-    //SendReport 
-
-    hid.send(&output_report);
-      
-    
-
-    //Read a message
-
-    if(hid.read_nb(&input_report)){
-        for(int i=0;i<input_report.length;i++){
-            printf("%d\r",input_report.data[i]);
+     while(1){
+        if (usbSerial.readable()!=0){
+                 pcDebug.putc(usbSerial.getc());
+        }else{
+            usbSerial.putc(name);
         }
-    }
-
-    }
+     }
 }
-
-
