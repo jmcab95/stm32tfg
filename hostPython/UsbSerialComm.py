ba190682ec9c28aa141f1936311c3d65bbdfc8b9
jmcab95 <josemicabanillasmansilla@gmail.com>
2020-06-19 13:13:36 +0200

    Update Project

--- hostPython/UsbSerialComm.py
+++ hostPython/UsbSerialComm.py
@@ -3,9 +3,10 @@ import os
 
 ser = serial.Serial('/dev/ttyACM1')
 
-
+sof = '\n'
+eof= '\t'
 while True:
     ser.write(b'HelloWorld')
 
     if(ser.readable):
-        print(ser.read())
+        #print(ser.read())
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "mbed.h"
 #include "USBSerial.h"
 #include "drivers/USBHID.h"
+#include "nanopb_library/pb_common.h"
+#include "nanopb_library/pb_decode.h"
+#include "nanopb_library/pb_encode.h"
+#include "nanopb_library/messages.pb.h"
+
 
 
 //Declare Serial Uart
@@ -18,10 +23,22 @@ char name = 'H';
 int main()
 {
      while(1){
+          
         if (usbSerial.readable()!=0){
-                 pcDebug.putc(usbSerial.getc());
-        }else{
-            usbSerial.putc(name);
+            switch(usbSerial.getc()){
+
+
+                // pcDebug.putc(usbSerial.getc());
         }
+            //usbSerial.putc(name);
+        
+        
+        
+
+
+        }
+
+
+
      }
 }
