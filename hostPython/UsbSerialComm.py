5a8a7f0e1ce946aa6797cd1455d4675cb3aaa23d
jmcab95 <josemicabanillasmansilla@gmail.com>
2020-06-26 20:01:26 +0200

    Update Code decode message from python

--- hostPython/UsbSerialComm.py
+++ hostPython/UsbSerialComm.py
@@ -1,12 +1,89 @@
 import serial
 import os 
+import sys
 
-ser = serial.Serial('/dev/ttyACM1')
+from protobuf import messagesPrueba_pb2
+from time import sleep
 
-sof = '\n'
-eof= '\t'
+
+def SerialConn():
+    port = '/dev/ttyACM1'
+    baudrate = 9600
+    try:
+        ser = serial.Serial(port,baudrate)
+        print('Conexión establecida')
+    except serial.SerialException:
+        ser.close()
+        ser.open()
+        print('Conexión establecida')
+    
+    return ser
+
+def ConsoleFill(messages):
+    
+    messages.digitalPin = int(input('Introduce un entero: (Digital Input)'))
+    messages.analogPin = int(input('Introduce otro entero (Analog Input): '))
+    messages.codigo = int(input('Introduce un codigo entero(Codigo): '))
+    value = input('Selecciona un valor... house, web, number, video: ')
+
+    if value == 'house':
+        messages.new_values = messagesPrueba_pb2.MensajePruebasPySTM.NewValues.HOUSE
+    elif value == 'web':
+        messages.new_values = messagesPrueba_pb2.MensajePruebasPySTM.NewValues.WEB
+    elif value == 'number':
+        messages.new_values = messagesPrueba_pb2.MensajePruebasPySTM.NewValues.NUMBER
+    elif value == 'video':
+        messages.new_values = messagesPrueba_pb2.MensajePruebasPySTM.NewValues.VIDEO
+    else:
+        messages.new_values = messagesPrueba_pb2.MensajePruebasPySTM.NewValues.DEFAULT
+
+    
+#port= '/dev/ttyACM1'
+
+
+messages = messagesPrueba_pb2.MensajePruebasPySTM()
+
+messages.digitalPin=564
+messages.analogPin = 987
+messages.codigo=125
+messages.new_values = messagesPrueba_pb2.MensajePruebasPySTM.NewValues.WEB
+
+#ConsoleFill(messages)
+flag = 0xaa
+final= 0xba
+
+message_bytes = messages.SerializeToString()
+
+flag_bytes=bytes([flag])
+final_flag_bytes=bytes([final])
+length_bytes=bytes([len(message_bytes)])
+
+
+
+
+
+conn = SerialConn()
+
+
 while True:
-    ser.write(b'HelloWorld')
+    print("Mandamos cabeceras")
+    conn.write(flag_bytes)
+    print("Mandamos cabeceras 2")
+    conn.write(flag_bytes)
+    print("Mandamos longitud")
+    conn.write(length_bytes)
+    print("Mandamos mensaje")
+    conn.write(message_bytes)
+    print("Mandamos cabecera final")
+    conn.write(final_flag_bytes)
 
-    if(ser.readable):
-        #print(ser.read())
+
+    
+            
+       
+
+
+
+    
+    
+
--- main.cpp
+++ main.cpp
@@ -1,44 +1,80 @@
-#include "mbed.h"
 #include "USBSerial.h"
 #include "drivers/USBHID.h"
+#include "mbed.h"
+#include "nanopb_library/messagesPrueba.pb.h"
 #include "nanopb_library/pb_common.h"
 #include "nanopb_library/pb_decode.h"
 #include "nanopb_library/pb_encode.h"
-#include "nanopb_library/messages.pb.h"
 
-
-
-//Declare Serial Uart
-
-Serial pcDebug(USBTX,USBRX);
-//Declare USBSerial Device
+Serial pcDebug(USBTX, USBRX);
 USBSerial usbSerial;
-
-char name = 'H';
-
-
-
+MensajePruebasPySTM mensajePrueba;
+size_t message_length = 128;
+bool status;
+DigitalOut ledIn(LED1);
 
 // main() runs in its own thread in the OS
-int main()
-{
-     while(1){
+int main() {
+  
+  char temp;
+
+  while (1) {
+    ledIn.write(0);
+    int i = 0;
+    if (usbSerial.readable()) {
+
+      if ((temp = usbSerial.getc()) == 0xaa) {
+        //pcDebug.printf("Entra en primer flag");
+        if ((temp = usbSerial.getc()) == 0xaa) {
+          //pcDebug.printf("Entra en segundo flag");
+          // Segundo flag
+          temp=usbSerial.getc();
+          //pcDebug.printf("Valor lenght leido: %d",temp);
+          message_length = temp;
+          uint8_t buf[message_length];
           
-        if (usbSerial.readable()!=0){
-            switch(usbSerial.getc()){
+          while (((temp = usbSerial.getc()) != 0xba) and (i < message_length)) {
+            //pcDebug.printf("Valor actual %02X", temp);
+            buf[i] = temp;
+            i++;
+          }
+          
 
+          pb_istream_t stream = pb_istream_from_buffer(buf, message_length);
+          status =
+              pb_decode(&stream, MensajePruebasPySTM_fields, &mensajePrueba);
 
-                // pcDebug.putc(usbSerial.getc());
+          if (!status) {
+            pcDebug.printf("Decoding failed %s\n", PB_GET_ERROR(&stream));
+            return 1;
+          }
+
+          pcDebug.printf("el msg del parametro es %d",
+                         mensajePrueba.digitalPin);
         }
-            //usbSerial.putc(name);
-        
-        
-        
+      }
+    } /*
+    else {
+      // Enviamos un mensaje protobuffer
 
+      pb_ostream_t stream = pb_ostream_from_buffer(buf, sizeof(buf));
+      mensajePrueba.digitalPin = 25;
+      mensajePrueba.analogPin = 20;
+      mensajePrueba.codigo=30;
 
-        }
+      status = pb_encode(&stream, MensajePruebasPySTM_fields,
+                         &mensajePrueba);
+      message_length = stream.bytes_written;
 
+      if (!status) {
+        pcDebug.printf("Encoding failed %s\n", PB_GET_ERROR(&stream));
+        return 1;
+      } else {
+        pcDebug.printf("Mensaje %02x", buf);
+      }
 
-
-     }
+      usbSerial.write(&buf, sizeof(buf));
+      }
+    }*/
+  }
 }
